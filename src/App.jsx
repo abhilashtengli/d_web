419de319664c71aa8c88a2d0d0e40e74bee4f464
Abhilash <abhilasht0007.t@gmail.com>
2024-09-26 07:27:38 +0530

    Code Setup.. : )

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./Body";
+import Login from "./Components/Login";
+import Profile from "./Components/Profile";
+
 function App() {
   return (
     <>
-      <div data-theme="dark" className="h-screen">
-        Hello
+      <div className="h-screen">
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   );
