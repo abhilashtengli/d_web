06f0f5f2d6f3281eff251354ddea7242aff3671d
Abhilash <abhilasht0007.t@gmail.com>
2024-09-29 16:24:28 +0530

    Fetched Feed Data and stored in the redux.

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,5 +1,29 @@
+import axios from "axios";
+import { Base_Url } from "../utils/Constants";
+import { useDispatch } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import { useEffect } from "react";
+
 const Feed = () => {
-  return <div>Feed</div>;
+  const dispatch = useDispatch();
+
+  const getFeed = async () => {
+    try {
+      const res = await axios.get(Base_Url + "/user/feed", {
+        withCredentials: true,
+      });
+      console.log(res?.data);
+
+      dispatch(addFeed(res?.data));
+    } catch (err) {
+      return err.message;
+    }
+  };
+  useEffect(() => {
+    getFeed();
+  }, []);
+
+  return <div></div>;
 };
 
 export default Feed;
